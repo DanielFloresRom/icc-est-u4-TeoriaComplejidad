042cc9e0d8224879a82975fc8a3520f81e8f87e3
Juanda <juandac98@outlook.com>
2026-04-23 20:15:12 -0500

    Tercera Parte

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // new ComplejidadConstante().ejemplo();
         // new ComplejidadLineal().ejemplo();
-        // new ComplejidadCuadratica().ejemplo();
+        new ComplejidadCuadratica().ejemplo();
         // new ComplejidadLogaritmica().ejemplo();
         // new ComplejidadNLogN().ejemplo();
     }
--- src/ComplejidadCuadratica.java
+++ src/ComplejidadCuadratica.java
@@ -1,3 +1,23 @@
 public class ComplejidadCuadratica {
-    
+    // Este tipo de complejidad ve como aumenta el tiempo de ejecucion proportionalmente al cuadrado del tamaño de la entrada.
+    // Esto significa que si el tamaño de la entrada se duplica, el tiempo de ejecución se cuadruplica por eso es la notacion O(n²).
+
+    // Un ejemplo claro es un algoritmo que compara los elemntos de una lista, como el algoritmo de ordenamiento de burbuja.
+
+    public void ejemplo() {
+    int[] n = {5, 10, 3};  //n es el tamaño de la entrada
+    int tam = n.length;
+    int operaciones=0;
+    for (int i = 0; i < tam; i++) {
+        for (int j = 0; j < tam; j++) {
+            operaciones++;
+        }
+    }
+    System.out.println("Operaciones: " + (operaciones)); // Esto muestra el número de operaciones realizadas, que es proporcional a n²
+
+    //En este caso, el numero de operaciones no es fijo ya que depende del tamaño de la entrada la cual se elevaria al cuadrado (n²).
+    //Se repetiran siempre las mismas lineas de codigo pero el numero de veces que se ejecutaran dependera del tamaño de la entrada.
+
+    }
 }
+
